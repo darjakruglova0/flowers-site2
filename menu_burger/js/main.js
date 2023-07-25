9af55e36b3d483d45ff3d92b07d53c2376450a72
Dasha Kachur <36744584+darjakruglova0@users.noreply.github.com>
2023-07-25 20:25:43 +0300

    Add files via upload

---
+++ menu_burger/js/main.js
@@ -0,0 +1,9 @@
+
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', function(){
+	burger.classList.toggle('active');
+	menu.classList.toggle('active');
+
+});
